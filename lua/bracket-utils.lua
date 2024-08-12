16cf67ab1aa033b7cb6cb15eb83bec68a30ed995
Luis Flak <64930654+luis-txt@users.noreply.github.com>
2024-08-13 00:51:01 +0200

    removed confusing bracket-skip

--- lua/bracket-utils.lua
+++ lua/bracket-utils.lua
@@ -101,32 +101,6 @@ function M.remove_matching_bracket()
 end
 
 --[[ 
-  Closing already closed brackets
-  
-  Behavior:
-  (|) and ) is entered => ()|
---]]
-function M.close_brackets(char)
-  local cursor = vim.api.nvim_win_get_cursor(0)
-  local line = vim.api.nvim_get_current_line()
-  local col = cursor[2]
-  local next_char = string.sub(line, col + 1, col + 1)
-
-  -- cursor is between two matching brackets or quotation marks
-  if char == next_char then
-    -- move cursor to the right
-    vim.api.nvim_feedkeys(
-      vim.api.nvim_replace_termcodes("<Right>", true, false, true),
-      "i", true
-    )
-    return
-  end
-
-  -- just print the entered character
-  vim.api.nvim_put({char}, '', false, true)
-end
-
---[[ 
   Fancy enter in brackets
 
   Behavior:
--- lua/brackets.lua
+++ lua/brackets.lua
@@ -9,8 +9,3 @@ vim.keymap.set('i', '\'', function() bracket_utils.handle_quotes('\'') end)
 vim.keymap.set('i', '"', function() bracket_utils.handle_quotes('"') end)
 vim.keymap.set('i', '<BS>', function() bracket_utils.remove_matching_bracket() end)
 vim.keymap.set('i', '<CR>', function() bracket_utils.space_enter() end)
-
-local chars_to_close = {')', '}', ']'}
-for _, char in ipairs(chars_to_close) do
-  vim.keymap.set('i', char, function() bracket_utils.close_brackets(char) end)
-end
