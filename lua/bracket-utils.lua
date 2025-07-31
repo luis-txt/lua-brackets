828685ac8ed782c976b1eb7259f5b09214d4368d
Luis <64930654+luis-txt@users.noreply.github.com>
2025-07-31 15:28:40 +0200

    fixed skipping quotation marks

--- lua/bracket-utils.lua
+++ lua/bracket-utils.lua
@@ -1,137 +1,97 @@
---[[ 
-  Collection of additions and simplifications for the use of parentheses
-  and quotation marks
---]]
-local M = { }
+-- Collection of smart auto-pairing and skipping
+local M = {}
 
 local bracket_pairs = {
-  ['('] = ')',
-  ['['] = ']',
-  ['{'] = '}',
-  ['"'] = '"',
-  ["'"] = "'",
+	['('] = ')', ['['] = ']', ['{'] = '}',
+	["\""] = "\"", ["'"] = "'"
 }
 
---[[
-	Check wether cursor is in front of a whitespace. If so it inserts both opening and closing
-	bracket. Else the cursor is in front of text. In that case it just inserts the typed bracket.
+local function feed_keys_helper(keys)
+	vim.api.nvim_feedkeys(
+		vim.api.nvim_replace_termcodes(keys, true, false, true),
+		'n', true
+	)
+end
 
-	Behavior:
-	| and ( is entered => (|)
-	|x and ( is entered => (|x
---]]
+function M.skip_close_pair()
+	feed_keys_helper('<Right>')
+end
+
 function M.handle_open_bracket(char)
-	local cursor = vim.api.nvim_win_get_cursor(0)
-  local line = vim.api.nvim_get_current_line()
-  local col = cursor[2]
-  local next_char = string.sub(line, col + 1, col + 1)
-	local curr_char = string.sub(line, col, col)
+	local _, col = unpack(vim.api.nvim_win_get_cursor(0))
+	local line = vim.api.nvim_get_current_line()
+	local next = line:sub(col+1, col+1)
+	if bracket_pairs[char] == next then
+		-- next is the matching closer, skip
+		return M.skip_close_pair()
+	end
+	if next == '' or next:match('%s') then
+		-- whitespace or empty, insert pair
+		return feed_keys_helper(char .. bracket_pairs[char] .. '<Left>')
+	end
+	-- just insert opener
+	return feed_keys_helper(char)
+end
 
-  if next_char == '' or next_char == '\n' or next_char == '\t' or curr_char == '' then
-		-- cursor in front of whitespace
-		vim.api.nvim_feedkeys(
-			vim.api.nvim_replace_termcodes(char .. bracket_pairs[char] .. "<Left>", true, false, true),
-			"n", true
-		)
+function M.handle_close_bracket(char)
+	local _, col = unpack(vim.api.nvim_win_get_cursor(0))
+	local line = vim.api.nvim_get_current_line()
+	local next = line:sub(col+1, col+1)
+	if next == char then
+		return M.skip_close_pair()
 	else
-		-- cursor in front of text
+		return feed_keys_helper(char)
+	end
+end
+
+function M.handle_quotes(char)
+	local _, col = unpack(vim.api.nvim_win_get_cursor(0))
+	local line = vim.api.nvim_get_current_line()
+	local next = line:sub(col+1, col+1)
+	if next == char then
+		return M.skip_close_pair()
+	else
+		return feed_keys_helper(char .. char .. '<Left>')
+	end
+end
+
+-- Backspace: delete empty pair or single
+function M.remove_matching_bracket()
+	local _, col = unpack(vim.api.nvim_win_get_cursor(0))
+	local line = vim.api.nvim_get_current_line()
+	local prev = line:sub(col, col)
+	local next = line:sub(col+1, col+1)
+	if bracket_pairs[prev] == next then
+		return feed_keys_helper('<BS><Del>') -- delete both
+	else
+		return feed_keys_helper('<BS>')
+	end
+end
+
+-- Enter inside pair: newline + indent + newline
+function M.space_enter()
+	local cursor = vim.api.nvim_win_get_cursor(0)
+	local line = vim.api.nvim_get_current_line()
+	local col = cursor[2]
+	local prev_char = string.sub(line, col, col)
+	local next_char = string.sub(line, col + 1, col + 1)
+	local matching_closing = bracket_pairs[prev_char]
+
+	-- cursor is between two matching brackets or quotation marks
+	if matching_closing and next_char == matching_closing then
+		-- make two enter and the fitting indentation between them
 		vim.api.nvim_feedkeys(
-			vim.api.nvim_replace_termcodes(char, true, false, true),
+			vim.api.nvim_replace_termcodes("<CR><Up><End><CR>", true, false, true),
 			"n", true
 		)
-  end
-end
+		return
+	end
 
---[[ 
-  Additional implementation for quotations because they behave different 
-  (since the open and closed character is the same)
-
-  Behavior:
-  | and " is entered => "|"
-  "|" and " is entered => ""|
---]]
-function M.handle_quotes(char)
-  local cursor = vim.api.nvim_win_get_cursor(0)
-  local line = vim.api.nvim_get_current_line()
-  local col = cursor[2]
-  local next_char = string.sub(line, col + 1, col + 1)
-
-  -- no already opened quotation marks
-  if char ~= next_char then
-    -- add two quotation marks and set the cursor between them
-    vim.api.nvim_feedkeys(
-      vim.api.nvim_replace_termcodes(char .. char .. "<Left>", true, false, true),
-      "n", true
-    )
-  else
-    -- move cursor to the right
-    M.close_brackets(char)
-  end
-end
-
---[[ 
-  Remove matching brackets when return between brackets
-
-  Behavior:
-  (|) and return is entered => |
---]]
-function M.remove_matching_bracket()
-  local cursor = vim.api.nvim_win_get_cursor(0)
-  local line = vim.api.nvim_get_current_line()
-  local col = cursor[2]
-  local char = string.sub(line, col, col)
-  local matching_closing = bracket_pairs[char]
-  local next_char = string.sub(line, col + 1, col + 1)
-
-  -- cursor is between two matching brackets or quotation marks
-  if matching_closing and matching_closing == next_char then
-    -- remove the opening and closing bracket or both quotation marks
-    vim.api.nvim_feedkeys(
-      vim.api.nvim_replace_termcodes("<BS><Del>", true, false, true),
-      "n", true
-    )
-    return
-  end
-
-  -- remove bracket or quotation mark to the left of the cursor
-  vim.api.nvim_feedkeys(
-    vim.api.nvim_replace_termcodes("<BS>", true, false, true),
-    "n", true
-  )
-end
-
---[[ 
-  Fancy enter in brackets
-
-  Behavior:
-  (|) and enter is entered =>
-  (
-  <<TAB TAB>>
-  )
---]]
-function M.space_enter()
-  local cursor = vim.api.nvim_win_get_cursor(0)
-  local line = vim.api.nvim_get_current_line()
-  local col = cursor[2]
-  local prev_char = string.sub(line, col, col)
-  local next_char = string.sub(line, col + 1, col + 1)
-  local matching_closing = bracket_pairs[prev_char]
-
-  -- cursor is between two matching brackets or quotation marks
-  if matching_closing and next_char == matching_closing then
-    -- make two enter and the fitting indentation between them
-    vim.api.nvim_feedkeys(
-      vim.api.nvim_replace_termcodes("<CR><Up><End><CR>", true, false, true),
-      "n", true
-    )
-    return
-  end
-
-  -- just insert an enter
-  vim.api.nvim_feedkeys(
-    vim.api.nvim_replace_termcodes("<CR>", true, false, true),
-    "n", true
-  )
+	-- just insert an enter
+	vim.api.nvim_feedkeys(
+		vim.api.nvim_replace_termcodes("<CR>", true, false, true),
+		"n", true
+	)
 end
 
 return M
--- lua/brackets.lua
+++ lua/brackets.lua
@@ -1,11 +1,23 @@
 local bracket_utils = require('bracket-utils')
+local opts = {noremap=true, silent=true}
 
--- auto pair
-vim.keymap.set('i', '(', function() bracket_utils.handle_open_bracket('(') end)
-vim.keymap.set('i', '{', function() bracket_utils.handle_open_bracket('{') end)
-vim.keymap.set('i', '[', function() bracket_utils.handle_open_bracket('[') end)
+-- Openers
+vim.keymap.set('i', '(', function() bracket_utils.handle_open_bracket('(') end, opts)
+vim.keymap.set('i', '[', function() bracket_utils.handle_open_bracket('[') end, opts)
+vim.keymap.set('i', '{', function() bracket_utils.handle_open_bracket('{') end, opts)
 
-vim.keymap.set('i', '\'', function() bracket_utils.handle_quotes('\'') end)
-vim.keymap.set('i', '"', function() bracket_utils.handle_quotes('"') end)
-vim.keymap.set('i', '<BS>', function() bracket_utils.remove_matching_bracket() end)
-vim.keymap.set('i', '<CR>', function() bracket_utils.space_enter() end)
+-- Closers
+vim.keymap.set('i', ')', function() bracket_utils.handle_close_bracket(')') end, opts)
+vim.keymap.set('i', ']', function() bracket_utils.handle_close_bracket(']') end, opts)
+vim.keymap.set('i', '}', function() bracket_utils.handle_close_bracket('}') end, opts)
+
+-- Quotes
+vim.keymap.set('i', '"', function() bracket_utils.handle_quotes('"') end, opts)
+vim.keymap.set('i', "'", function() bracket_utils.handle_quotes("'") end, opts)
+
+-- Backspace and Enter
+vim.keymap.set('i', '<BS>', bracket_utils.remove_matching_bracket, opts)
+vim.keymap.set('i', '<CR>', bracket_utils.space_enter, opts)
+
+-- Right arrow skips any closer
+vim.keymap.set('i', '<Right>', bracket_utils.close_pair, opts)
